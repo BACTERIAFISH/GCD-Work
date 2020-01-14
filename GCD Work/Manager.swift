54feb527bd60e4a1e9095d6c96c1a7575e83faa6
Bacteriafish <you@example.com>
2020-01-14 14:54:19 +0800

    complete group and semaphore

---
+++ GCD Work/Manager.swift
@@ -0,0 +1,88 @@
+//
+//  Manager.swift
+//  GCD Work
+//
+//  Created by FISH on 2020/1/14.
+//  Copyright © 2020 FISH. All rights reserved.
+//
+
+import Foundation
+
+class Manager {
+    
+    let url = "https://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=5012e8ba-5ace-4821-8482-ee07c147fd0a&limit=1&offset="
+    
+    func fetch(offset: Int, completion: @escaping (Result<SCSpeed, Error>) -> Void) {
+        let api = "\(url)\(offset)"
+        
+        guard let url = URL(string: api) else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(Result.failure(error))
+                }
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, let data = data else {
+                return
+            }
+            
+            switch response.statusCode {
+            case 200..<300:
+                let decoder = JSONDecoder()
+                do {
+                    let result = try decoder.decode(SCResponse.self, from: data)
+                    //print(result.result)
+                    DispatchQueue.main.async {
+                        completion(Result.success(result.result))
+                    }
+                } catch {
+                    DispatchQueue.main.async {
+                        completion(Result.failure(error))
+                    }
+                }
+                
+            default:
+                print("unexpected error")
+//                DispatchQueue.main.async {
+//                    completion(Result.failure(error))
+//                }
+            }
+            
+        }
+        
+        task.resume()
+    }
+    
+}
+
+struct SCResponse: Codable {
+    let result: SCSpeed
+}
+
+struct SCSpeed: Codable {
+    let limit: Int
+    let offset: Int
+    let count: Int
+    let sort: String
+    let results: [SCSpeedResult]
+}
+
+struct SCSpeedResult: Codable {
+    let functions: String
+    let area: String
+    let no: String
+    let direction: String
+    let speedLimit: String
+    let location: String
+    let id: Int
+    let road: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case functions, area, no, direction, location, road
+        case speedLimit = "speed_limit"
+        case id = "_id"
+    }
+}
--- GCD Work/ViewController.swift
+++ GCD Work/ViewController.swift
@@ -9,12 +9,164 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var groupRoadLabel0: UILabel!
+    @IBOutlet weak var groupLimitLabel0: UILabel!
+    @IBOutlet weak var groupRoadLabel10: UILabel!
+    @IBOutlet weak var groupLimitLabel10: UILabel!
+    @IBOutlet weak var groupRoadLabel20: UILabel!
+    @IBOutlet weak var groupLimitLabel20: UILabel!
+    
+    @IBOutlet weak var semaphoreRoadLabel0: UILabel!
+    @IBOutlet weak var semaphoreLimitLabel0: UILabel!
+    @IBOutlet weak var semaphoreRoadLabel10: UILabel!
+    @IBOutlet weak var semaphoreLimitLabel10: UILabel!
+    @IBOutlet weak var semaphoreRoadLabel20: UILabel!
+    @IBOutlet weak var semaphoreLimitLabel20: UILabel!
+    
+    let manager = Manager()
+    
+    var groupRoad0 = ""
+    var groupLimit0 = ""
+    var groupRoad10 = ""
+    var groupLimit10 = ""
+    var groupRoad20 = ""
+    var groupLimit20 = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
+    @IBAction func startGroup(_ sender: UIButton) {
+        
+        cleanGroup()
+        
+        let group = DispatchGroup()
+        
+        DispatchQueue.global().async(group: group) { [weak self] in
+            group.enter()
+            self?.manager.fetch(offset: 0) { result in
+                switch result {
+                case .success(let data):
+                    self?.groupRoad0 = data.results[0].road
+                    self?.groupLimit0 = data.results[0].speedLimit
+                    group.leave()
+                case .failure(let error):
+                    print(error)
+                    group.leave()
+                }
+            }
+        }
+        
+        DispatchQueue.global().async(group: group) { [weak self] in
+            group.enter()
+            self?.manager.fetch(offset: 10) { result in
+                switch result {
+                case .success(let data):
+                    self?.groupRoad10 = data.results[0].road
+                    self?.groupLimit10 = data.results[0].speedLimit
+                    group.leave()
+                case .failure(let error):
+                    print(error)
+                    group.leave()
+                }
+            }
+        }
+        
+        DispatchQueue.global().async(group: group) { [weak self] in
+            group.enter()
+            self?.manager.fetch(offset: 20) { result in
+                switch result {
+                case .success(let data):
+                    self?.groupRoad20 = data.results[0].road
+                    self?.groupLimit20 = data.results[0].speedLimit
+                    group.leave()
+                case .failure(let error):
+                    print(error)
+                    group.leave()
+                }
+            }
+        }
+        
+        group.notify(queue: DispatchQueue.main) { [weak self] in
+            self?.groupRoadLabel0.text = self?.groupRoad0
+            self?.groupLimitLabel0.text = self?.groupLimit0
+            self?.groupRoadLabel10.text = self?.groupRoad10
+            self?.groupLimitLabel10.text = self?.groupLimit10
+            self?.groupRoadLabel20.text = self?.groupRoad20
+            self?.groupLimitLabel20.text = self?.groupLimit20
+        }
+    }
+    
+    @IBAction func startSemaphore(_ sender: UIButton) {
+        
+        cleanSemaphore()
+        
+        let semaphore = DispatchSemaphore(value: 1)
+        
+        DispatchQueue.global().async { [weak self] in
+            semaphore.wait()
+            self?.manager.fetch(offset: 0) { result in
+                switch result {
+                case .success(let data):
+                    self?.semaphoreRoadLabel0.text = data.results[0].road
+                    self?.semaphoreLimitLabel0.text = data.results[0].speedLimit
+                    semaphore.signal()
+                case .failure(let error):
+                    print(error)
+                    semaphore.signal()
+                }
+            }
+        }
+        
+        DispatchQueue.global().async { [weak self] in
+            semaphore.wait()
+            self?.manager.fetch(offset: 10) { result in
+                switch result {
+                case .success(let data):
+                    self?.semaphoreRoadLabel10.text = data.results[0].road
+                    self?.semaphoreLimitLabel10.text = data.results[0].speedLimit
+                    semaphore.signal()
+                case .failure(let error):
+                    print(error)
+                    semaphore.signal()
+                }
+            }
+        }
+        
+        DispatchQueue.global().async { [weak self] in
+            semaphore.wait()
+            self?.manager.fetch(offset: 20) { result in
+                switch result {
+                case .success(let data):
+                    self?.semaphoreRoadLabel20.text = data.results[0].road
+                    self?.semaphoreLimitLabel20.text = data.results[0].speedLimit
+                    semaphore.signal()
+                case .failure(let error):
+                    print(error)
+                    semaphore.signal()
+                }
+            }
+        }
+    }
+    
+    func cleanGroup() {
+        groupRoadLabel0.text = ""
+        groupRoadLabel10.text = ""
+        groupRoadLabel20.text = ""
+        groupLimitLabel0.text = ""
+        groupLimitLabel10.text = ""
+        groupLimitLabel20.text = ""
+    }
+    
+    func cleanSemaphore() {
+        semaphoreRoadLabel0.text = ""
+        semaphoreRoadLabel10.text = ""
+        semaphoreRoadLabel20.text = ""
+        semaphoreLimitLabel0.text = ""
+        semaphoreLimitLabel10.text = ""
+        semaphoreLimitLabel20.text = ""
+    }
 }
 
